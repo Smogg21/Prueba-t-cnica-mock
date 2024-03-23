3dacb54432d8428805e01aebe43e6cf6c153432e
Smogg21 <spazzaglia98@gmail.com>
2024-03-23 13:11:35 -0500

    Added image to the random fact

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 export function App () {
-  const [fact, setFact] = useState('')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((response) => response.json())
-      .then((data) => setFact(data.fact))
+      .then((data) => {
+        const { fact } = data
+        setFact(fact)
+        const fourFirstWords = fact.split(' ', 4).join(' ')
+        setImageUrl(`https://cataas.com/cat/says/${fourFirstWords}?size=50&fontColor=red`)
+      })
   }, [])
 
   return (
     <main>
-      <h1>Hola Causas</h1>
+      <h1>Random Facts de gatos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first word of ${fact}`} />}
     </main>
   )
 }
