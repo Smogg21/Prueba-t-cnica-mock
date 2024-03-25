4cd8c79fdcab953fe52d499c8dcff8c26804f518
Smogg21 <spazzaglia98@gmail.com>
2024-03-25 17:36:58 -0500

    Continued working on project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+import { getRandomFact } from './services/facts'
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((response) => response.json())
-      .then((data) => {
-        const { fact } = data
-        setFact(fact)
-      })
+    getRandomFact().then(newFact => setFact(newFact))
   }, [])
 
   useEffect(() => {
@@ -23,13 +17,9 @@ export function App () {
     setImageUrl(CAT_ENDPOINT_IMAGE)
   }, [fact])
 
-  const handleClick = () => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((response) => response.json())
-      .then((data) => {
-        const { fact } = data
-        setFact(fact)
-      })
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
   }
 
   return (
