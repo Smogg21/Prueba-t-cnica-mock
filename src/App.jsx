33e23398bcb41901824b1381340d41a0627eef65
Smogg21 <spazzaglia98@gmail.com>
2024-03-25 18:07:27 -0500

    Cat image is now set as a custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import { getRandomFact } from './services/facts'
 
-export function App () {
-  const [fact, setFact] = useState()
+function useCatImage ({ fact }) {
   const [imageUrl, setImageUrl] = useState()
-
-  useEffect(() => {
-    getRandomFact().then(newFact => setFact(newFact))
-  }, [])
-
   useEffect(() => {
     if (!fact) return
     const fourFirstWords = fact.split(' ', 4).join(' ')
     const CAT_ENDPOINT_IMAGE = `https://cataas.com/cat/says/${fourFirstWords}?size=50&fontColor=red`
     setImageUrl(CAT_ENDPOINT_IMAGE)
   }, [fact])
+
+  return { imageUrl }
+}
+
+export function App () {
+  const [fact, setFact] = useState()
+  const { imageUrl } = useCatImage({ fact })
+
+  useEffect(() => {
+    getRandomFact().then(newFact => setFact(newFact))
+  }, [])
 
   const handleClick = async () => {
     const newFact = await getRandomFact()
